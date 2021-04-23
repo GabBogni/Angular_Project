aa58d4341770ccefafa08aa12b2eaa969ac76082
Gabriel <gabriel96kt@gmail.com>
2021-04-22 21:26:10 -0300

    Componente adicionado e aparecendo no html principal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Component2Component } from './component2/component2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Component2Component
   ],
   imports: [
     BrowserModule
